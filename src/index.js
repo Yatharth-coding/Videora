73c16d88291a6299838e4b284662b0528bb07ed5
Yatharth Yadav <yatharthcoding@gmail.com>
2026-02-11 01:04:31 +0530

    starting with a project

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./env'
+});
+
+connectDB() ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express();
+
+;( async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (err)=>{
+            console.log("Error occured in index.js" , err);
+            throw err;
+        })
+
+        app.listen(process.env.PORT , ()=>{
+            console.log(`App is listening at port ${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.log("error is" , error);
+        throw err ;
+    }
+})
+    */
